a7868e0617f41663f0573e995d43ed538927e9f0
djoni1vincent <djoni8vincent@gmail.com>
2026-01-15 11:31:53 +0100

    feat: add login page, using flask_wtf for forms

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,8 @@
+from pickle import GET
+from flask import render_template, flash, redirect
 from app import app
-from flask import render_template
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -16,3 +19,12 @@ def index():
     ]
 
     return render_template('index.html', title="Home", user=user, posts=posts)
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect("/index")
+    return render_template("login.html", title="Sign In", form=form)
